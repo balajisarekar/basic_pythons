ce78859bc764812cfe3417fbd302cd704e49551a
Balaji sarekar <balajisarekar1993@gmail.com>
2026-07-22 01:23:02 +0530

    ffff

---
+++ fibonaccii_seriesnumber.py
@@ -0,0 +1,10 @@
+#wap to fiboncacci series
+def fibonacci_series(n):
+    a,b=0,1
+    for i in range(n):
+        print(a,end=" ")
+        a,b=b,a+b
+
+n=int(input("Enter the number: "))
+fibonacci_series(n)
+
