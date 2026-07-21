941df2ccf108594ebba362ebd3c3e7973b191cd5
Balaji sarekar <balajisarekar1993@gmail.com>
2026-07-22 00:53:07 +0530

    n -u "c:\Users\HP\OneDrive\Desktop\codepython\basic_pythons\palindrome.py" git n -u "c:\Users\HP\OneDrive\Desktop\codepython\basic_pythons\palindrome.py" Add palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+# write a program for palindrome number
+n=int(input("Enter a number:"))
+temp=n
+rev=0
+while(n>0):
+    digit=n%10
+    rev=rev*10+digit
+    n=n//10
+if(temp==rev):
+    print("the number is palindrome")
+else:
+    print("the number is not palindrome")
+    
